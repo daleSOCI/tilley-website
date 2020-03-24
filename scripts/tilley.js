af5085ae0f7416656642b6304e8e5ac648e0455f
dalespiteri <dalespiteri@gmail.com>
2020-03-24 12:18:11 -0400

    Collapsable Header

---
+++ scripts/tilley.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    // Collapsable Header
+    // On window scroll
+    $(window).scroll(function () {
+        var scrollPos = $(this).scrollTop();
+        // If not at the top of the page
+        if (scrollPos > 0) {
+            // Collpase the header
+            $('header').addClass('collapsed');
+        } else {
+            // Otherwise, show the full header
+            $('header').removeClass('collapsed');
+        }
+    });
+
+});
